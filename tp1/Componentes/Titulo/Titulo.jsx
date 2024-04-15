6e3a1e8a5f1f624772eca1619aeeec2e6b2862a5
UrraPablo <UrraPablo@users.noreply.github.com>
2024-04-14 22:07:29 -0300

    Estuctura de archivos

---
+++ tp1/Componentes/Titulo/Titulo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Titulo = () => {
+  return (
+    <h1>Titulo Tp1</h1>
+  );
+}
+
--- tp1/src/App.js
+++ tp1/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Titulo } from '../Componentes/Titulo/Titulo';
 
 function App() {
   return (
@@ -15,9 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pablo
         </a>
       </header>
+      <Titulo/>
     </div>
   );
 }
